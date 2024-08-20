6b171a136e25d3a466704161ead75b456f11ef82
Edmund Miller <git@edmundmiller.dev>
2024-08-20 12:37:39 -0500

    chore: Copy over all the SQL statements
    https://github.com/nf-core/website/blob/old-site/public_html/stats.php

---
+++ sources/nfcore_db/all_repos.sql
@@ -0,0 +1,1 @@
+SELECT * FROM nfcore_pipelines
---
+++ sources/nfcore_db/gh_contributors.sql
@@ -0,0 +1,4 @@
+SELECT DISTINCT author, avatar_url, SUM(week_commits) AS total_sum_commits
+FROM github_pipeline_contrib_stats
+GROUP BY author, avatar_url
+ORDER BY total_sum_commits DESC
