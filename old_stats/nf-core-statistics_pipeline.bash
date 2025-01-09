a3f5f7ab0365e99be9056e4d3ec247587652432a
Edmund Miller <edmund.miller@seqera.io>
2025-01-09 13:49:10 -0600

    fix(#31): Pull out all of the metrics tracked

--- old_stats/nf-core-statistics_pipeline.bash
+++ old_stats/nf-core-statistics_pipeline.bash
@@ -2,18 +2,25 @@
 git clone https://github.com/MatthiasZepper/nf-core-statistics.git
 cd nf-core-statistics
 
-# Use Python conversion function to extract the data we want
+# Use Python conversion function to extract all metrics
 uvx git-history file nf-core-stats.db metrics.json \
 --convert '
 try:
     data = json.loads(content)
     return [{
         "id": "metrics",
+        "namesOfAdopters": data.get("namesOfAdopters"),
+        "namesOfContributors": data.get("namesOfContributors"),
+        "namesOfContributorsNew": data.get("namesOfContributorsNew"),
         "numberOfPullRequestNew": data.get("numberOfPullRequestNew"),
         "p50NumberOfNewPullsPerWeek": data.get("p50NumberOfNewPullsPerWeek"),
-        "meanNumberOfNewPullsPerWeek": data.get("meanNumberOfNewPullsPerWeek"),
+        "p50NumberOfNewContributorsPerWeek": data.get("p50NumberOfNewContributorsPerWeek"),
         "p50SecondsToClosePulls": data.get("p50SecondsToClosePulls"),
-        "meanSecondsToClosePulls": data.get("meanSecondsToClosePulls")
+        "p50SecondsToCloseIssues": data.get("p50SecondsToCloseIssues"),
+        "meanNumberOfNewPullsPerWeek": data.get("meanNumberOfNewPullsPerWeek"),
+        "meanNumberOfNewContributorsPerWeek": data.get("meanNumberOfNewContributorsPerWeek"),
+        "meanSecondsToClosePulls": data.get("meanSecondsToClosePulls"),
+        "meanSecondsToCloseIssues": data.get("meanSecondsToCloseIssues")
     }]
 except Exception as ex:
     return []
