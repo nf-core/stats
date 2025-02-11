0c5dec5a94ae1044fd8368de823795d244327071
mashehu <mashehu3@gmail.com>
2025-02-11 10:04:28 +0100

    send messages to iframe parent

--- pages/+layout.svelte
+++ pages/+layout.svelte
@@ -2,7 +2,29 @@
   import "@evidence-dev/tailwind/fonts.css";
   import "../app.css";
   import { EvidenceDefaultLayout } from "@evidence-dev/core-components";
+  import { onMount } from 'svelte';
+  import { page } from '$app/stores';
+
   export let data;
+
+  // Send URL changes to parent window of the iframe
+  $: if (typeof window !== 'undefined' && window.parent !== window) {
+    const path = $page.url.pathname;
+    window.parent.postMessage({
+      type: 'urlChange',
+      path: path
+    }, 'https://nf-co.re');
+  }
+
+  onMount(() => {
+    // Send initial URL to the parent window of the iframe
+    if (window.parent !== window) {
+      window.parent.postMessage({
+        type: 'urlChange',
+        path: window.location.pathname
+      }, 'https://nf-co.re');
+    }
+  });
 </script>
 
 <EvidenceDefaultLayout
