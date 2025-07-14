b61574ba5a41d0857e684baf39102be165bd1f3e
mashehu <mashehu3@gmail.com>
2025-07-14 13:34:28 +0200

    remove unused sources

--- sources/nfcore_db/repo_traffic_leaderboard.sql
+++ sources/nfcore_db/repo_traffic_leaderboard.sql
@@ -3,12 +3,12 @@ USE nf_core_stats_bot;
 SELECT 
     t.pipeline_name as repository,
     'https://github.com/nf-core/' || t.pipeline_name as repository_link,
-    SUM(t.views) as total_views,
-    SUM(t.views_uniques) as total_views_unique,
-    SUM(t.clones) as total_clones,
-    SUM(t.clones_uniques) as total_clones_unique,
-    p.stargazers_count
+    SUM(COALESCE(t.views, 0)) as total_views,
+    SUM(COALESCE(t.views_uniques, 0)) as total_views_unique,
+    SUM(COALESCE(t.clones, 0)) as total_clones,
+    SUM(COALESCE(t.clones_uniques, 0)) as total_clones_unique,
+    COALESCE(p.stargazers_count, 0) as stargazers_count
 FROM github.traffic_stats t
 LEFT JOIN github.nfcore_pipelines p ON t.pipeline_name = p.name
 GROUP BY t.pipeline_name, p.stargazers_count, p.name
-ORDER BY total_views DESC 
+ORDER BY total_views DESC
